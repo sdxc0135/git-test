97f82bbb10593d073239493730bec0ff965a0e2e
송시한 <103095971+sdxcsdxc@users.noreply.github.com>
2025-08-22 16:42:23 +0900

    수정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 
 export default {
   async fetch(request, env, ctx) {
-    await env.KV_APP.put("hello", "world");
-    const v = await env.KV_APP.get("hello");
-    return new Response(v);
+    return new Response('Hello Wor11ld!');
   }
 };
